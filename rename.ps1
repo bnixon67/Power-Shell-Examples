de55939b828d163c145b758306cb2d459af61733
Bill Nixon <bnixon67@gmail.com>
2020-06-07 13:18:35 -0500

    Add files via upload

---
+++ rename.ps1
@@ -0,0 +1,30 @@
+$regexp='^Statement_(?<MM>[0-9]{2})-(?<DD>[0-9]{2})-(?<YYYY>[0-9]{4}).pdf'
+
+Get-ChildItem | Where-Object { $_.Name -match $regexp } | ForEach {
+
+  $YYYY = $Matches.YYYY
+  $MM   = $Matches.MM
+  $DD   = $Matches.DD
+
+  $NewName = "$YYYY-$MM-$DD Nordstrom.pdf"
+
+  Write-Host $_.Name, "->", $NewName
+
+  $_ | Rename-Item -NewName $NewName
+
+}
+
+$regexp='^VisaAnnualSummary_(?<YYYY>[0-9]{4}).pdf'
+
+Get-ChildItem | Where-Object { $_.Name -match $regexp } | ForEach {
+
+  $YYYY = $Matches.YYYY
+  
+
+  $NewName = "$YYYY-12-31 Nordstrom Annual Summary.pdf"
+
+  Write-Host $_.Name, "->", $NewName
+
+  $_ | Rename-Item -NewName $NewName
+
+}
