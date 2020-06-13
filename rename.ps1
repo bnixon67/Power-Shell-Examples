a9796196de39701912021bccc3eecf74461d9b1c
Bill Nixon <bnixon67@gmail.com>
2020-06-13 13:01:07 -0500

    Added simple version and comments

--- rename.ps1
+++ rename.ps1
@@ -1,7 +1,13 @@
+# Simple version
+# you will need to adjust the regexp and the replace string to match what you what to do
+$regexp='(?<YYYY>[0-9]{4})(?<MM>[0-9]{2})(?<DD>[0-9]{2})_OTHER_TEXT.pdf'
+Get-ChildItem | Where-Object { $_.Name -match $regexp } | Rename-Item -NewName { $_.Name -replace $regexp, '${YYYY}-${MM}-${DD} UPDATED TEXT.pdf' }
+
+# Complex version, which allows some more control and displays the actions
+# You can comment out the Rename-Item action to test what will occur
 $regexp='^Statement_(?<MM>[0-9]{2})-(?<DD>[0-9]{2})-(?<YYYY>[0-9]{4}).pdf'
 
 Get-ChildItem | Where-Object { $_.Name -match $regexp } | ForEach {
-
   $YYYY = $Matches.YYYY
   $MM   = $Matches.MM
   $DD   = $Matches.DD
@@ -11,20 +17,16 @@ Get-ChildItem | Where-Object { $_.Name -match $regexp } | ForEach {
   Write-Host $_.Name, "->", $NewName
 
   $_ | Rename-Item -NewName $NewName
-
 }
 
 $regexp='^VisaAnnualSummary_(?<YYYY>[0-9]{4}).pdf'
 
 Get-ChildItem | Where-Object { $_.Name -match $regexp } | ForEach {
-
   $YYYY = $Matches.YYYY
-  
 
   $NewName = "$YYYY-12-31 VENDOR Annual Summary.pdf"
 
   Write-Host $_.Name, "->", $NewName
 
   $_ | Rename-Item -NewName $NewName
-
 }
